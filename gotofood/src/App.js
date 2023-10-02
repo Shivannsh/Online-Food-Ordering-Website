2f364b811657644a74cfb941e2984194606a0262
Shivannsh <bb1221392@iitd.ac.in>
2023-10-02 07:29:03 +0530

    Basic Structure Made

---
+++ gotofood/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './Screens/Home';
+import Login from './Screens/Login';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/login' element={<Login/>}/>
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ gotofood/src/Screens/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+export default function Login() {
+  return (
+    <div>
+        <Navbar/>
+        <h2>Login Kar Bhai</h2>
+    </div>
+  )
+}
