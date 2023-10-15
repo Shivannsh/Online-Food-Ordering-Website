1ea1d183eb1108911b6cc0895eae787582638ab3
Shivannsh <bb1221392@iitd.ac.in>
2023-10-15 13:29:04 +0530

    Encryption Added JWT and bcrypt

--- gotofood/Backend/Routes/Createuser.js
+++ gotofood/Backend/Routes/Createuser.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router()
-
 const User = require("../Models/User")
 const { body, validationResult } = require("express-validator");
-
+const bcrypt = require("bcrypt");
+const saltrounds=10;
+const jwt = require("jsonwebtoken");
 
 router.post("/createuser",
 
@@ -18,14 +19,50 @@ router.post("/createuser",
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
+
+        const hash= await bcrypt.hash(req.body.password,saltrounds);
+
         try {
             await User.create({
                 name: req.body.name,
                 location: req.body.location,
                 email: req.body.email,
-                password: req.body.password,
+                password: hash,
             })
             res.json({ success: true })
+        } catch (error) {
+            console.log(error);
+            res.json({ success: false })
+        }
+    })
+
+
+router.post("/login",
+    [body('email').isEmail(),
+    body("password", "Incorect PAssword").isLength({ min: 5 })],
+
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });}
+        try {
+            const userData = await User.findOne({ email: req.body.email })
+            if (!userData) {
+                return res.status(400).json({ errors: [{ msg: "User not found" }] })
+            }
+            const isMatch = await bcrypt.compare(req.body.password, userData.password);
+            if (!isMatch) {
+                return res.status(400).json({ errors: [{ msg: "Password Incorrect" }] })
+            }
+
+            const payload = {
+                user: {
+                    id: userData.id
+                }
+            }
+            const Authtoken= jwt.sign(payload,"12345678912345678123456781234567812345678")
+
+            res.json({ success: true ,Authtoken:Authtoken})
         } catch (error) {
             console.log(error);
             res.json({ success: false })
--- gotofood/src/Screens/Login.jsx
+++ gotofood/src/Screens/Login.jsx
@@ -1,11 +1,96 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
+import React, { useState } from "react";
+import Navbar from "../Components/Navbar";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [credentials, setcredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handlechange = (event) => {
+    const { name, value } = event.target;
+    setcredentials({ ...credentials, [name]: value });
+  };
+
+  const handleclick = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
+    const json_backend = await response.json();                      // response.json() is a promise/ response cominf from server side is in json format
+    console.log(json_backend);                                       // yh aapko console mei dekhne ko milega ki json mei kya kya hai
+    if (!json_backend.success) {
+                                          // if success is false then it will show alert(jaise backend p humne likha hai json==sucess to vo data aaya or us data ka naam bhi json hi h to isliye json check kiya)
+                                         //save the auth token and redirect
+      alert("Invalid Credentials");
+    } else {
+      localStorage.setItem("token", json_backend.Authtoken); // yh token ko local storage mei save kr dega
+      console.log(localStorage.getItem("token"));
+      navigate("/");
+    }
+  };
+
   return (
     <div>
-        <Navbar/>
-        <h2>Login Kar Bhai</h2>
+      <Navbar />
+      <div className="container">
+        <form onSubmit={handleclick}>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              name="email"
+              value={credentials.email}
+              onChange={handlechange}
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              name="password"
+              value={credentials.password}
+              onChange={handlechange}
+            />
+          </div>
+
+          {/* <div className="mb-3">
+    <label htmlFor="exampleInputPassword1" className="form-label">Confirm Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
+  </div> */}
+
+          <button type="submit" className="m-3 btn btn-success">
+            Submit
+          </button>
+
+          <Link to="/createuser" className="m-3 btn btn-danger">
+            Not an existing user? Sign Up
+          </Link>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
